d375e07faac7e58b99a54d7bc34dbb466ed8e92d
Jigao Luo <luojigao@outlook.com>
2021-11-29 20:44:55 +0800

    disable perf on insertion.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- third_party/ART/ART.cpp
+++ third_party/ART/ART.cpp
@@ -622,16 +622,16 @@ int main(int argc,char** argv) {
    // Build tree
    double start = gettime();
    Node* tree=NULL;
-   PerfEvent e;
-   e.startCounters();
+//   PerfEvent e;
+//   e.startCounters();
    for (uint64_t i=0;i<n;i++) {
       uint8_t key[8];loadKey(keys[i],key);
       insert(tree,&tree,key,0,keys[i],8);
    }
    printf("insert,%ld,%f\n",n,(n/1000000.0)/(gettime()-start));
-   e.stopCounters();
-   e.printReport(std::cout, n); // use n as scale factor
-   std::cout << std::endl;
+//   e.stopCounters();
+//   e.printReport(std::cout, n); // use n as scale factor
+//   std::cout << std::endl;
 
     /// Prepare to-be-looked-up keys w.r.t. the distribution program argument
     uint64_t* lookup_keys=new uint64_t[n];
