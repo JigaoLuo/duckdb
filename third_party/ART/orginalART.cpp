42a8beb3b7b3c6a9850f3f2343b09a4b702e71c8
Jigao Luo <luojigao@outlook.com>
2021-11-29 17:10:47 +0800

    comment out vec&set to original art.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- third_party/ART/orginalART.cpp
+++ third_party/ART/orginalART.cpp
@@ -694,8 +694,8 @@ int main(int argc,char** argv) {
             set.emplace(index);
             lookup_keys[i] = keys[index]; /// Fix zipfian distribution's value range to [0, n)
         }
-        std::cout << "lookup indexes as vector: " << std::endl; for (const auto& ele : vec)  std::cout << ele << std::endl;
-        std::cout << "lookup indexes as set: #=" << set.size() << std::endl; for (const auto& ele : set)  std::cout << ele << std::endl;
+        // std::cout << "lookup indexes as vector: " << std::endl; for (const auto& ele : vec)  std::cout << ele << std::endl;
+        // std::cout << "lookup indexes as set: #=" << set.size() << std::endl; for (const auto& ele : set)  std::cout << ele << std::endl;
     }
 
    // Repeat lookup for small trees to get reproducable results
