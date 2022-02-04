f3aef654fb7f456496e548377f529f2eec091023
Jigao Luo <luojigao@outlook.com>
2022-02-04 22:37:08 +0800

    change bash.
    Signed-off-by: Jigao Luo <luojigao@outlook.com>

--- art_standalone/run.sh
+++ art_standalone/run.sh
@@ -4,7 +4,7 @@ output_file=output.dat
 rm -f $output_file
 touch $output_file
 
-for i in $(seq 0.0 0.2 3.0)
+for i in $(seq 0.0 0.05 3.0)
 do
   ../build/release/art_standalone/art_standalone 1000000 0 z $i 2>&1 | tee $output_file # sorted
 #  ../build/release/art_standalone/art_standalone 1000000 2 z $i 2>&1 | tee $output_file # sparse
